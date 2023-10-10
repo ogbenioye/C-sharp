77736a47edb13c4a973693e844b49fe9bf3b9f88
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-10-10 09:05:30 +0100

    commit 09

---
+++ 13ReferencesLesson/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+
+        public string ScrapeWebPage(string url)
+        {
+            return GetPage(url);
+        }
+
+        public string ScrapeWebPage(string url, string filePath)
+        {
+            string reply = GetPage(url);
+            File.WriteAllText(filePath, reply);
+
+            return reply;
+        }
+
+        private string GetPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+
+    }
+}
