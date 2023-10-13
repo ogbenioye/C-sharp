4bd13041dcba3828ad784403e560c9854b638350
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-10-13 12:13:42 +0100

    assemblies and refrences example

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyCodeLibrary;
+
+namespace MyClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Scrape myScrape = new Scrape();
+           string value = myScrape.ScrapeWebPage("https://msdn.microsoft.com");
+
+            Console.WriteLine(value);
+            Console.ReadLine(); 
+        }
+    }
+}
